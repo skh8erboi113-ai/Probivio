fd94f80accbe3a9b56d65cc4d91081f6f42dc813
Brenden <sk.h8er.boi113@gmail.com>
2026-07-01 19:25:09 -0700

    Add ESLint configuration for TypeScript project

---
+++ .eslintrc.cjs
@@ -0,0 +1,81 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    project: false,
+  },
+  plugins: ['@typescript-eslint', 'import', 'security'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:security/recommended-legacy',
+    'prettier',
+  ],
+  env: {
+    node: true,
+    es2022: true,
+  },
+  rules: {
+    // TypeScript strictness
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-non-null-assertion': 'error',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
+    ],
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+
+    // Import ordering
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index', 'type'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+    'import/no-default-export': 'off',
+    'import/no-cycle': 'error',
+    'import/no-self-import': 'error',
+
+    // Security
+    'security/detect-object-injection': 'off',
+    'security/detect-non-literal-fs-filename': 'warn',
+
+    // General code quality
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
+    'no-debugger': 'error',
+    'no-alert': 'error',
+    'prefer-const': 'error',
+    'no-var': 'error',
+    eqeqeq: ['error', 'always'],
+    curly: ['error', 'multi-line'],
+    'no-return-await': 'error',
+    'require-await': 'error',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+      node: true,
+    },
+  },
+  ignorePatterns: ['node_modules', 'dist', 'build', '.turbo', 'coverage', '*.config.js', '*.config.cjs'],
+};
